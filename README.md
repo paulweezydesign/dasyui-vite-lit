930260554f59e4877a57ec319bc9536f7c5ac8af
paulweezydesign <weezy@paulweezydesign.com>
2022-04-21 23:14:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# daisyui-lit-vite-kgpd97
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/daisyui-lit-vite-kgpd97)
---
+++ src/my-element.js
@@ -0,0 +1,17 @@
+import './app.css';
+
+import { html, LitElement } from 'lit';
+
+export class MyElement extends LitElement {
+  createRenderRoot() {
+    return this; // turn off shadow dom to access external styles
+  }
+  render() {
+    return html`
+      <h1 class="text-6xl">Hello Vite + Lit </h1>
+      <button class="btn">Hello daisyUI</button>
+    `;
+  }
+}
+
+window.customElements.define('my-element', MyElement);
